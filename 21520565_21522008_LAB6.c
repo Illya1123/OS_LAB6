d0bf479be027e9d301222c6b463a83608963a0dc
IllyaChanNQAnh <21520565@gm.uit.edu.vn>
2023-06-14 14:51:28 +0700

    add variable default

--- 21520565_21522008_LAB6.c
+++ 21520565_21522008_LAB6.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 
+int column = 0, frame = 0;
+char default_ref[] ={'2','1','5','2','0','5','6','5','0','0','7'}; // MSSV là 21520565; 
+int page_fault = 0;
+#define MAX_SIZE 100
+
+char arr[MAX_SIZE][MAX_SIZE];
+
+
+
 int main()
 {
     return 0;
